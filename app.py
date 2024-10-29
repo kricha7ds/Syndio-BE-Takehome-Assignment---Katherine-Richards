ba35ec18b154a88584761f5570f8e74e9e4a3a9c
Katherine Richards <kricha7ds@gmail.com>
2024-10-29 06:39:13 -0600

    Add configurable port

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello, World!"
+
+if __name__ == '__main__':
+  port = int(os.environ.get('FLASK_PORT', 5000))
+  app.run(port=port, debug=True)
