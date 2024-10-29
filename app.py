48ef7f0d965780064448afe481f7b75c991c2a11
Katherine Richards <kricha7ds@gmail.com>
2024-10-29 08:32:34 -0600

    Improve error handling

--- app.py
+++ app.py
@@ -20,11 +20,7 @@ def db_connection():
   finally:
     connection.close()
 
-@app.route("/")
-def hello():
-  return "Hello, World!"
-
-def calculate_mlr(department: str):
+def calculate_mlr(department):
   with db_connection() as conn:
     try:
       sql = """
@@ -38,6 +34,9 @@ def calculate_mlr(department: str):
       """
       df = pd.read_sql_query(sql, conn, params=(department,))
 
+      if df.empty:
+        return None
+
       y = df.compensation
       df = pd.get_dummies(df, columns=['protected_class'], drop_first=True, dtype=int)
       X = df.drop(columns=["compensation"]).assign(const=1)
@@ -46,20 +45,26 @@ def calculate_mlr(department: str):
       pvalues = model.pvalues
 
       return pvalues.get('protected_class_reference', pvalues.get('protected_class_comparison'))
+
     except sqlite3.Error:
-      return None
+      raise
 
 @app.route("/pvalue")
 def pvalue():
   department = request.args.get('department', '').capitalize()
+  try:
+    result = calculate_mlr(department)
 
-  result = calculate_mlr(department)
+    if result is None:
+      return jsonify({"success": False, "error": "Not found"}), 404
 
-  if result:
     pvalue = round(result, 3)
+
     return jsonify({"pvalue": pvalue})
-  else:
-    return jsonify({"status": 404, "message": "Not Found"}), 404
+
+  except Exception as e:
+    return jsonify({"success": False, "error": "Internal server error"}), 500
+
 
 if __name__ == "__main__":
   port = int(os.environ.get("FLASK_PORT", 5000))
