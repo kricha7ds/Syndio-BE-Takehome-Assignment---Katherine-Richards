c08f6cba3a29cc9e481de10712403b7b6eaf5b25
Katherine Richards <kricha7ds@gmail.com>
2024-10-29 07:55:33 -0600

    calculate ols model, return pvalue for protected_class

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from dotenv import load_dotenv
 import sqlite3
 from contextlib import contextmanager
 
+import statsmodels.api as sm
+import pandas as pd
+
 load_dotenv()
 
 app = Flask(__name__)
-VALID_DEPARTMENTS=[]
 
 @contextmanager
 def db_connection():
   connection = sqlite3.connect('employees.db')
-  connection.row_factory = sqlite3.Row # give access to row results
+  connection.row_factory = sqlite3.Row
   try:
     yield connection
   finally:
@@ -22,18 +24,43 @@ def db_connection():
 def hello():
   return "Hello, World!"
 
-def calculate_mlr(department): # Multiple linear regression
-  return
+def calculate_mlr(department: str):
+  with db_connection() as conn:
+    try:
+      sql = """
+        SELECT tenure, performance, protected_class, compensation
+        FROM employees
+        WHERE department = ?
+          AND tenure IS NOT NULL
+          AND performance IS NOT NULL
+          AND protected_class IS NOT NULL
+          AND compensation IS NOT NULL
+      """
+      df = pd.read_sql_query(sql, conn, params=(department,))
+
+      y = df.compensation
+      df = pd.get_dummies(df, columns=['protected_class'], drop_first=True, dtype=int)
+      X = df.drop(columns=["compensation"]).assign(const=1)
+
+      model = sm.OLS(y, X).fit()
+      pvalues = model.pvalues
+
+      return pvalues.get('protected_class_reference', pvalues.get('protected_class_comparison'))
+    except sqlite3.Error:
+      return None
 
 @app.route("/pvalue")
 def pvalue():
   department = request.args.get('department', '').capitalize()
 
-  results = calculate_mlr(department)
+  result = calculate_mlr(department)
 
-  # return jsonify({"pvalue": results})
-  return jsonify({"success": "success"})
+  if result:
+    pvalue = round(result, 3)
+    return jsonify({"pvalue": pvalue})
+  else:
+    return jsonify({"status": 404, "message": "Not Found"}), 404
 
-if __name__ == '__main__':
-  port = int(os.environ.get('FLASK_PORT', 5000))
+if __name__ == "__main__":
+  port = int(os.environ.get("FLASK_PORT", 5000))
   app.run(port=port, debug=True)
